802ab43508af60e72ac59320d64ee9e07fc00885
취업반PM <PM@ITBANK-01>
2015-05-11 21:44:57 +0900

    launching..

---
+++ jse/src/cmm02/op/No04_ConditionDemo.java
@@ -0,0 +1,15 @@
+package cmm02.op;
+
+public class No04_ConditionDemo {
+	public static void main(String[] args) {
+		int val01 = 1;
+		int val02 = 2;
+		
+		if((val01 == 1) && (val02 == 2)){
+			System.out.println("1번값과 2번값은  same !! ");
+		}
+		if((val01 != 1) || (val02 != 2)){
+			System.out.println("1번값과 2번값은 different !!");
+		}
+	}
+}
