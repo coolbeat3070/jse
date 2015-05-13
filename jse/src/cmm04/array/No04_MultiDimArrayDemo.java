49ec56edd7ce102f274c3b97d42dfdbc993530eb
취업반PM <PM@ITBANK-01>
2015-05-13 21:32:42 +0900

    ..

--- jse/src/cmm04/array/No04_MultiDimArrayDemo.java
+++ jse/src/cmm04/array/No04_MultiDimArrayDemo.java
@@ -4,7 +4,7 @@ public class No04_MultiDimArrayDemo {
 	public static void main(String[] args) {
 		String[][] names = {
 				{"Mr.","Mrs.","Ms."},
-				{"Smith","John"}
+				{"Smith","Johnn"}
 		};
 		System.out.println(names[0][0]+names[1][0]); // Mr.Smith
 		System.out.println(names[0][2]+names[1][1]); // Ms.John
